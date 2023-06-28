4e16552ca814b17c43d9771f10f48876c8013d52
dondapatisaiprakash <135398401+dondapatisaiprakash@users.noreply.github.com>
2023-06-27 23:14:37 -0400

    python.py

---
+++ python.py
@@ -0,0 +1,27 @@
+# string.py
+#string
+string="welcome to python"
+x=string.lower()
+print(x)
+#welcome to python
+x=string.upper()
+print(x)
+#WELCOME TO PYTHON
+x=string.capitalize()
+print(x)
+#Welcome to python
+x=string.encode()
+print(x)
+#b'welcome to python'
+x=string.isupper()
+print(x)
+#False
+x=string.islower()
+print(x)
+#True
+x=string.istitle()
+print(x)
+#False
+x=string.isidentifier()
+print(x)
+#False
